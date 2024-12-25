0562b8b4c877cc51239275e21332e67c8efb19e6
TakHoya <130663193+Hoya0718@users.noreply.github.com>
2024-12-25 19:50:06 +0900

    img 임시제거

--- bibletodolist/src/Components/Login/Login.js
+++ bibletodolist/src/Components/Login/Login.js
@@ -90,10 +90,10 @@ return (
                 <img src="/img/카카오.png" alt="카카오 로그인" />카카오로 로그인
             </button> */}
             <button className="social-login-btn naver" onClick={() => OAuth2Login("naver")}>
-                <img src="/img/naver.png" alt="네이버 로그인" />네이버로 로그인
+                <p>네이버로 로그인</p>
             </button>
             <button className="social-login-btn google" onClick={() => OAuth2Login("google")}>
-                <img src="/img/google.png" alt="구글 로그인" /><p>구글로 로그인</p>
+                <p>구글로 로그인</p>
             </button>
         </div>
     </div>
